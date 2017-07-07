a536bc8253682bcddeb20312c13977834d2687a0
yeqiu <yeqiudef@gmail.com>
2017-07-07 18:53:45 +0800

    清理iframe

---
+++ NoIframe.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python
+#coding:utf-8
+
+import itertools
+
+def createDeck():
+    I=[1,2,3,4,5,6,7,8,9,10]
+    J=['spades','hearts','diamonds','clubs']
+    return (list(itertools.product(I,J)))
+
+
+print createDeck()
