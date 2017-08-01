7cfcab5c72141a2df22a7577611218b992efd2d6
yeqiu <yeqiudef@gmail.com>
2017-08-01 18:05:06 +0800

    彩票

--- TentTool.py
+++ TentTool.py
@@ -4,17 +4,18 @@ import re
 
 class TentTool(object):
     divPattern = re.compile('<div.*?>|</div>')
-    pPattern = re.compile('<p.*>')
+    pPattern = re.compile('<p.*?>')
     spanPattern = re.compile('<span.*?>|</span>')
     h1Pattern = re.compile('<h1.*?>')
-    imgPattern = re.compile('<img.*?src="(.*?)".*?>')
-
+    figurePattern = re.compile('<figure.*?>|</figure>')
+    figcaptionPattern = re.compile('<figcaption.*?>|</figcaption>')
     def remove_tags(self,content):
         content = re.sub(self.divPattern,'',content)
         content = re.sub(self.pPattern,'<p>',content)
         content = re.sub(self.spanPattern,'',content)
         content = re.sub(self.h1Pattern,'<h1>',content)
-        content = re.sub(self.imgPattern,'<img src="https://guanjia.qq.com\g<1>">',content)
+        content = re.sub(self.figurePattern,'',content)
+        content = re.sub(self.figcaptionPattern,'',content)
         return content
 
 
--- tqcp.py
+++ tqcp.py
@@ -2,6 +2,13 @@
 #coding:utf-8
 import urllib2
 import re
+import TentTool
+import urllib
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+tentTool = TentTool.TentTool()
 
 # 获取页面内容 指定网站编码
 def get_page(url, charcode):
@@ -20,13 +27,50 @@ def get_page(url, charcode):
         return page.read().decode(charcode)
 
 def get_content(url):
+    global tentTool
     page = get_page(url,'gbk')
     pattern = re.compile('<div class="font_14" style="line-height:180%;">(.*?)</div>',re.S)
     result = re.search(pattern,page)
     if not result:
-        print None
+        text = ''
     else:
-        print result.group(1).strip()
+        content = result.group(1).strip()
+        text = tentTool.remove_tags(content)
+        # print content
+    pattern = re.compile('<div class="font_22 font_b" align="center">(.*?)</div>')
+    result = re.search(pattern,page)
+    if not result:
+        title = ''
+    else:
+        title = result.group(1).strip()
+    pattern = re.compile('<meta name="description" content="(.*?)" />',re.S)
+    result = re.search(pattern,page)
+    if not result:
+        smalltext = ''
+    else:
+        smalltext = result.group(1).strip()
+    postData(title,smalltext,text,title)
+
+
+# 登陆
+def postData(title, smalltext, text, keyboard):
+    jiekou = 'http://www.jdms8.com/e/admin/jiekou.php'
+    data = urllib.urlencode({
+        'username': 'yeqiu',
+        'classid': 1,
+        'title': title,
+        'keyboard': keyboard,
+        'smalltext': smalltext,
+        'infotags': '南国特区彩票,特区彩票七星彩论坛',
+        'newstext': text,
+        'pw': '123456'
+
+    })
+    # 创建request
+    request = urllib2.Request(jiekou, data)
+    resp = urllib2.urlopen(request)
+    if '成功' in resp.read():
+        print '成功'
 
 # 获取urls
 def get_urls(start,end):
@@ -43,7 +87,7 @@ def get_urls(start,end):
             items = re.findall(pattern,result.group(1).strip())
             for item in items:
                 get_content(item)
-                exit()
+                # exit()
             print '-------'
 
 
